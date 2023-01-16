7a678730551cb17e24e2c2e6a1bbdd7a45f26b49
Christhofer94 <christhoferxx@gmail.com>
2023-01-16 16:56:23 -0300

    js in botton theme and media screen

---
+++ script.js
@@ -0,0 +1,7 @@
+const check = document.getElementById('switch-shadow');
+check.addEventListener('change', ()=>{
+    document.body.classList.toggle('dark')
+})
+check.addEventListener('change', ()=>{
+    document.container-fluid.classList.toggle('dark')
+})
